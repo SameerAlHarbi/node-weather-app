461efa68e716c5f5d9be41c762dd3890ecb1b217
Sameer Al Harbi <smrhrbi@gmail.com>
2020-02-05 12:58:09 +0300

    error handling

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ request({ url : url, json: true }, (error, response) => {
 
     if(error) {
         console.log('Unable to connect to weather service !');
+    } else if(response.body.error) {
+        console.log('Unable to find location !');
     } else {
         console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.');
     }
@@ -21,7 +23,13 @@ request({ url : url, json: true }, (error, response) => {
 const geocodeUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Jeddah.json?access_token=pk.eyJ1IjoiYWJ1anVkZSIsImEiOiJjazY1anQyMXkwOGY5M21sdjdqczQ0enN0In0.BzIGjxWHVv_vP7eHlBqwQg&limit=1";
 
 request({url: geocodeUrl, json: true}, (error, response) => {
-    const latitude = response.body.features[0].center[1];
-    const longtude = response.body.features[0].center[0];
-    console.log(latitude, longtude);
+    if (error) {
+        console.log('Unable to connect to location services !')
+    } else if(response.body.features.length === 0) {
+        console.log('Unable to find location, Try another search')
+    } else {
+        const latitude = response.body.features[0].center[1];
+        const longtude = response.body.features[0].center[0];
+        console.log(latitude, longtude);
+    }
 });
